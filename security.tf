fcb593713a8d60b1b672b322b0d9ead78cf7afae
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-05 15:01:03 -0800

    Clean-up security groups

--- security.tf
+++ security.tf
@@ -9,16 +9,16 @@ resource "aws_security_group_rule" "allow_from_vpn" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  security_group_id = var.geff-lambda-access
+  security_group_id = aws_security_group.sentry_sg.id
 }
 
-resource "aws_security_group_rule" "allow_from_vpn" {
+resource "aws_security_group_rule" "allow_to_all" {
   type              = "egress"
   to_port           = 0
   from_port         = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.hive-lb.id
+  security_group_id = aws_security_group.sentry_sg.id
 }
 
 resource "aws_security_group_rule" "sentry_allow_from_vpn" {
