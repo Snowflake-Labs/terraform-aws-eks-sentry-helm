d5011d0a1bea887b11401d06bd77ca0fcc061489
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-05 15:04:57 -0800

    Clean-up security groups

--- security.tf
+++ security.tf
@@ -4,27 +4,19 @@ resource "aws_security_group" "sentry_sg" {
   vpc_id      = var.vpc_id
 }
 
-resource "aws_security_group_rule" "allow_from_vpn" {
+resource "aws_security_group_rule" "sentry_sg_allow_from_vpn" {
   type              = "ingress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
+  cidr_blocks       = var.allowed_cidr_blocks
   security_group_id = aws_security_group.sentry_sg.id
 }
 
-resource "aws_security_group_rule" "allow_to_all" {
+resource "aws_security_group_rule" "sentry_sg_allow_to_vpn" {
   type              = "egress"
   to_port           = 0
   from_port         = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sentry_sg.id
-}
-
-resource "aws_security_group_rule" "sentry_allow_from_vpn" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sentry_sg.id
