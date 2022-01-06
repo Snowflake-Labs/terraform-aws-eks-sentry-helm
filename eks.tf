bdaf152eaea46683ef7884d74c407eca061b9f96
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-05 21:26:58 -0800

    Added helm package based release

--- eks.tf
+++ eks.tf
@@ -20,18 +20,18 @@ module "eks" {
 
   worker_groups = [
     {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = ""
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.sentry_sg.id]
+      name                 = "worker-group-1"
+      instance_type        = "t2.small"
+      additional_userdata  = ""
+      asg_desired_capacity = 2
+      #   additional_security_group_ids = [aws_security_group.sentry_sg.id]
     },
     {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = ""
-      asg_desired_capacity          = 1
-      additional_security_group_ids = [aws_security_group.sentry_sg.id]
+      name                 = "worker-group-2"
+      instance_type        = "t2.medium"
+      additional_userdata  = ""
+      asg_desired_capacity = 1
+      #   additional_security_group_ids = [aws_security_group.sentry_sg.id]
     },
   ]
 
--- security.tf
+++ security.tf
@@ -1,23 +1,23 @@
-resource "aws_security_group" "sentry_sg" {
-  name        = "sentry-sg"
-  description = "controls access to kubernetes cluster."
-  vpc_id      = var.vpc_id
-}
+# resource "aws_security_group" "sentry_sg" {
+#   name        = "sentry-sg"
+#   description = "controls access to kubernetes cluster."
+#   vpc_id      = var.vpc_id
+# }
 
-resource "aws_security_group_rule" "sentry_sg_allow_from_vpn" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = var.allowed_cidr_blocks
-  security_group_id = aws_security_group.sentry_sg.id
-}
+# resource "aws_security_group_rule" "sentry_sg_allow_from_vpn" {
+#   type              = "ingress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = var.allowed_cidr_blocks
+#   security_group_id = aws_security_group.sentry_sg.id
+# }
 
-resource "aws_security_group_rule" "sentry_sg_allow_to_vpn" {
-  type              = "egress"
-  to_port           = 0
-  from_port         = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sentry_sg.id
-}
+# resource "aws_security_group_rule" "sentry_sg_allow_to_vpn" {
+#   type              = "egress"
+#   to_port           = 0
+#   from_port         = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.sentry_sg.id
+# }
