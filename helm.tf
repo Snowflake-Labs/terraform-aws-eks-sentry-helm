25c3d2ab506499b2b0c117521a0de1a5e10ecfe5
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-05 18:07:10 -0800

    Fix chart name.

--- helm.tf
+++ helm.tf
@@ -5,7 +5,7 @@ resource "kubernetes_namespace" "sentry" {
 }
 
 resource "helm_release" "sentry" {
-  name       = "sentryy"
+  name       = "sentry"
   repository = "https://sentry-kubernetes.github.io/charts"
   chart      = "sentry"
   namespace  = kubernetes_namespace.sentry.metadata[0].name
