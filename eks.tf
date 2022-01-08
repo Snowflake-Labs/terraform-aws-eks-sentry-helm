07e773d17b41e8c1394d065d1638c63be74adf08
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-07 19:54:32 -0800

    Fix node group defaults to t2.medium

--- eks.tf
+++ eks.tf
@@ -1,47 +1,3 @@
-# module "eks" {
-#   source = "terraform-aws-modules/eks/aws"
-
-#   cluster_name                                = var.eks_cluster_name
-#   cluster_version                             = var.kubernetes_version
-#   vpc_id                                      = module.gha_vpc.vpc_id
-#   subnets                                     = module.gha_vpc.private_subnet_ids
-#   cluster_endpoint_private_access             = var.cluster_endpoint_private_access
-#   cluster_endpoint_public_access_cidrs        = var.cluster_endpoint_public_access_cidrs
-#   kubeconfig_file_permission                  = var.kubeconfig_file_permission
-#   kubeconfig_output_path                      = var.kubeconfig_output_path
-#   write_kubeconfig                            = var.write_kubeconfig
-#   kubeconfig_aws_authenticator_command        = var.kubeconfig_aws_authenticator_command
-#   kubeconfig_name                             = var.kubeconfig_name
-#   kubeconfig_aws_authenticator_command_args   = var.kubeconfig_aws_authenticator_command_args
-#   kubeconfig_aws_authenticator_env_variables  = var.kubeconfig_aws_authenticator_env_variables
-#   cluster_enabled_log_types                   = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-#   cluster_log_retention_in_days               = 0
-
-
-
-#   node_groups_defaults = {
-#     ami_type  = "AL2_x86_64"
-#     disk_size = 50
-#   }
-
-#   node_groups = {
-#     node-group-1 = {
-#       desired_capacity = 6
-#       max_capacity     = 6
-#       min_capacity     = 6
-
-#       instance_types = ["t3.xlarge"]
-#       capacity_type  = "ON_DEMAND"
-#       k8s_labels = {
-#         Environment = "${var.env}"
-#       }
-#     }
-#   }
-
-#   map_users    = var.map_users
-#   map_accounts = var.map_accounts
-# }
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "17.24.0"
@@ -58,26 +14,24 @@ module "eks" {
   cluster_enabled_log_types     = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   cluster_log_retention_in_days = 0
 
-  workers_group_defaults = {
-    root_volume_type = "gp2"
+  node_groups_defaults = {
+    ami_type  = "AL2_x86_64"
+    disk_size = 50
   }
 
-  worker_groups = [
-    {
-      name                 = "worker-group-1"
-      instance_type        = "t2.medium"
-      additional_userdata  = ""
-      asg_desired_capacity = 2
-      #   additional_security_group_ids = [aws_security_group.sentry_sg.id]
-    },
-    {
-      name                 = "worker-group-2"
-      instance_type        = "t2.medium"
-      additional_userdata  = ""
-      asg_desired_capacity = 1
-      #   additional_security_group_ids = [aws_security_group.sentry_sg.id]
-    },
-  ]
+  node_groups = {
+    node-group-1 = {
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capacity     = 3
+
+      instance_types = ["t2.medium"]
+      capacity_type  = "ON_DEMAND"
+      k8s_labels = {
+        environment = "${var.env}"
+      }
+    }
+  }
 
   map_users    = var.map_users
   map_accounts = var.map_accounts
