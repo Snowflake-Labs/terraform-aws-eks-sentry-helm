240f111562015d38080b14cba109b5f00eea736f
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-18 14:52:11 -0800

    Testing

--- security.tf
+++ security.tf
@@ -23,27 +23,27 @@
 # }
 
 
-# Tines DB Security Group
-resource "aws_security_group" "sentry_rds_pg" {
-  name        = "sentry-rds-pg"
-  description = "Database security group for the Tines application"
-  vpc_id      = var.vpc_id
-}
+# # Sentry DB Security Group
+# resource "aws_security_group" "sentry_rds_pg" {
+#   name        = "sentry-rds-pg"
+#   description = "Database security group for the Tines application"
+#   vpc_id      = var.vpc_id
+# }
 
-resource "aws_security_group_rule" "sentry_rds_pg_allow_from_bastion" {
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  source_security_group_id = var.bastion_security_group_id
-  security_group_id        = aws_security_group.sentry_rds_pg.id
-}
+# resource "aws_security_group_rule" "sentry_rds_pg_allow_from_bastion" {
+#   type                     = "ingress"
+#   from_port                = 5432
+#   to_port                  = 5432
+#   protocol                 = "tcp"
+#   source_security_group_id = var.bastion_security_group_id
+#   security_group_id        = aws_security_group.sentry_rds_pg.id
+# }
 
-resource "aws_security_group_rule" "sentry_rds_pg_allow_from_eks_cluster" {
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  source_security_group_id = module.eks.cluster_security_group_id
-  security_group_id        = aws_security_group.sentry_rds_pg.id
-}
+# resource "aws_security_group_rule" "sentry_rds_pg_allow_from_eks_cluster" {
+#   type                     = "ingress"
+#   from_port                = 5432
+#   to_port                  = 5432
+#   protocol                 = "tcp"
+#   source_security_group_id = module.eks.cluster_security_group_id
+#   security_group_id        = aws_security_group.sentry_rds_pg.id
+# }
