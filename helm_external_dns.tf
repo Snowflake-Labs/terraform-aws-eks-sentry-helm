46edef229c017341fb375b7a3106df40820afc88
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-18 14:34:38 -0800

    Testing

--- helm_external_dns.tf
+++ helm_external_dns.tf
@@ -4,17 +4,17 @@ resource "helm_release" "external_dns" {
   repository = "https://charts.bitnami.com/bitnami"
   version    = "6.1.1"
 
-  namespace  = "kube-system"
+  namespace = "kube-system"
 
   values = [
     templatefile(
       "${path.module}/templates/external_dns_values.yaml",
       {
-        aws_region = "${var.aws_region}",
-        aws_zone_type = "${var.external_dns_zone_type}",
-        external_dns_eks_service_account = "${kubernetes_service_account.external_dns_service_account.metadata[0].name}",
-        aws_iam_role_external_dns = "${aws_iam_role.external_dns_role.name}",
-        aws_iam_role_external_dns_arn = "${aws_iam_role.external_dns_role.arn}",
+        aws_region                       = "${var.aws_region}",
+        aws_zone_type                    = "${var.external_dns_zone_type}",
+        external_dns_eks_service_account = "external-dns",
+        aws_iam_role_external_dns        = "${aws_iam_role.external_dns_role.name}",
+        aws_iam_role_external_dns_arn    = "${aws_iam_role.external_dns_role.arn}",
       }
     )
   ]
