26adae8acb6e4081676e2942bf050661bcc1730b
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-24 16:23:58 -0800

    Testing

--- eks.tf
+++ eks.tf
@@ -9,7 +9,7 @@ module "eks" {
   cluster_endpoint_public_access_cidrs = var.allowed_cidr_blocks
 
   vpc_id      = var.vpc_id
-  subnet_ids  = contact(var.private_subnet_ids, var.public_subnet_ids)
+  subnet_ids  = concat(var.private_subnet_ids, var.public_subnet_ids)
   enable_irsa = true
 
   cluster_enabled_log_types = [
