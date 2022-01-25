c5a166e0f549636907fc3e062df5e2f250efe636
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-24 17:49:56 -0800

    Testing

--- sentry_ingress.tf
+++ sentry_ingress.tf
@@ -24,14 +24,16 @@ resource "kubernetes_ingress" "sentry_ingress" {
     }
 
     annotations = {
-      "kubernetes.io/ingress.class"               = "alb"
-      "alb.ingress.kubernetes.io/scheme"          = "internet-facing"
-      "alb.ingress.kubernetes.io/target-type"     = "ip"
-      "alb.ingress.kubernetes.io/tags"            = "environment=${var.env}"
-      "alb.ingress.kubernetes.io/listen-ports"    = jsonencode([{ HTTP = 80 }]) #, HTTPS = 443
-      "alb.ingress.kubernetes.io/inbound-cidrs"   = "${join(",", var.allowed_cidr_blocks)}"
-      "alb.ingress.kubernetes.io/subnets"         = "${join(",", var.public_subnet_ids)}"
-      "external-dns.alpha.kubernetes.io/hostname" = local.sentry_dns_name
+      "kubernetes.io/ingress.class"                = "alb"
+      "alb.ingress.kubernetes.io/scheme"           = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type"      = "ip"
+      "alb.ingress.kubernetes.io/tags"             = "environment=${var.env}"
+      "alb.ingress.kubernetes.io/listen-ports"     = jsonencode([{ HTTP = 80 }]) #, HTTPS = 443
+      "alb.ingress.kubernetes.io/inbound-cidrs"    = "${join(",", var.allowed_cidr_blocks)}"
+      "alb.ingress.kubernetes.io/subnets"          = "${join(",", var.public_subnet_ids)}"
+      "external-dns.alpha.kubernetes.io/hostname"  = local.sentry_dns_name
+      "alb.ingress.kubernetes.io/healthcheck-port" = "traffic-port"
+      "alb.ingress.kubernetes.io/healthcheck-path" = "/_health/"
       # "alb.ingress.kubernetes.io/actions.ssl-redirect" = ""
       # "alb.ingress.kubernetes.io/certificate-arn" = var.subdomain_cert_arn
     }
