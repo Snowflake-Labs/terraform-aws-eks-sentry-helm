b98c0635a7bf659320822ebad7cb1ba006598418
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-01-26 19:35:26 -0800

    Add rds security groups and RDS instance module.

--- helm_sentry.tf
+++ helm_sentry.tf
@@ -23,10 +23,10 @@ resource "helm_release" "sentry" {
         private_subnet_ids_str  = "${join(",", var.private_subnet_ids)}",
         public_subnet_ids_str   = "${join(",", var.public_subnet_ids)}",
         tags                    = "environment=${var.env}"
-        # postgres_db_host        = "${module.sentry_rds_pg.this_rds_cluster_endpoint}",
-        # postgres_db_name        = "${local.db_name}",
-        postgres_username = "${local.db_user}",
-        postgres_password = "${local.db_pass}",
+        postgres_db_host        = "${module.sentry_rds_pg.this_rds_cluster_endpoint}",
+        postgres_db_name        = "${local.db_name}",
+        postgres_username       = "${local.db_user}",
+        postgres_password       = "${local.db_pass}",
       }
     )
   ]
