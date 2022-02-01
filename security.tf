4806c7fc13e3b4bcd51b5c4f6db1c2796449e10d
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-02-01 15:01:29 -0800

    Outputs.

--- security.tf
+++ security.tf
@@ -19,7 +19,7 @@ resource "aws_security_group_rule" "sentry_rds_pg_allow_from_eks_cluster" {
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
-  source_security_group_id = module.eks.cluster_security_group_id
+  source_security_group_id = var.cluster_security_group_id
   security_group_id        = aws_security_group.sentry_rds_pg.id
 }
 
@@ -28,6 +28,6 @@ resource "aws_security_group_rule" "sentry_rds_pg_allow_from_eks_node" {
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
-  source_security_group_id = module.eks.node_security_group_id
+  source_security_group_id = var.node_security_group_id
   security_group_id        = aws_security_group.sentry_rds_pg.id
 }
