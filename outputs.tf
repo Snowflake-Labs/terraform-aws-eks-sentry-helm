7de9976c10b92ee2da31ee8bd80c1cee41b541c9
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-05-13 21:50:48 -0700

    Add output.

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,6 @@
+output "sentry_private_lb_sg_id" {
+  description = "Private LB security group."
+  value       = aws_security_group.sentry_private_ingress_sg.0.id
 
+  depends_on = [aws_security_group.sentry_private_ingress_sg]
+}
