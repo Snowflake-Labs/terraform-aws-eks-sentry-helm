e3cf948b8016965b1fec81882e000f4495095f64
sfc-gh-esamsunder <edwin.samsunderb@snowflake.com>
2022-10-31 21:00:33 +0530

    sentry monitoring ++

--- helm_sentry.tf
+++ helm_sentry.tf
@@ -43,3 +43,119 @@ resource "helm_release" "sentry" {
     )
   ]
 }
+
+# ------------------------------------------------------------------------------
+# Monitoring Configuration
+# ------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "sentry_remote_write_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_provider}"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${var.oidc_provider}:sub"
+      values = [
+        "system:serviceaccount:${var.grafana_namespace}:${var.service_account_name}"
+      ]
+    }
+  }
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_provider}"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${var.oidc_provider}:sub"
+      values = [
+        "system:serviceaccount:${var.prometheus_namespace}:${var.service_account_name}"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "sentry_eks_amp_role" {
+  name               = var.service_account_iam_role_name
+  description        = var.service_account_iam_role_description
+  assume_role_policy = data.aws_iam_policy_document.sentry_remote_write_assume.json
+}
+
+resource "aws_iam_policy" "sentry_amp_write" {
+  name        = var.service_account_iam_policy_name
+  description = "Permissions to write to all AMP workspaces"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "aps:RemoteWrite",
+          "aps:QueryMetrics",
+          "aps:GetSeries",
+          "aps:GetLabels",
+          "aps:GetMetricMetadata"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "sentry_amp_write" {
+  role       = aws_iam_role.sentry_eks_amp_role.name
+  policy_arn = aws_iam_policy.sentry_amp_write.arn
+}
+
+resource "kubernetes_namespace" "prometheus" {
+  count = var.create_prometheus_server == true ? 1 : 0
+  metadata {
+    name = var.prometheus_namespace
+  }
+}
+
+data "template_file" "prometheus_values" {
+  count    = var.create_prometheus_server == true ? 1 : 0
+  template = file("${path.module}/templates/prometheus-for-amp.values")
+
+  vars = {
+    region    = var.aws_region
+    name      = var.service_account_name
+    arn       = aws_iam_role.sentry_eks_amp_role.arn
+    workspace = var.sentry_amp_workspace_id
+  }
+}
+
+resource "local_file" "prometheus_values" {
+  count    = var.create_prometheus_server == true ? 1 : 0
+  content  = data.template_file.prometheus_values[0].rendered
+  filename = "${path.module}/templates/prometheus_values.yaml"
+}
+
+resource "helm_release" "prometheus_install" {
+  count             = var.create_prometheus_server == true ? 1 : 0
+  name              = "prometheus-for-amp"
+  repository        = "https://prometheus-community.github.io/helm-charts"
+  chart             = "prometheus"
+  namespace         = var.prometheus_namespace
+  timeout           = 600
+  wait              = false
+  dependency_update = true
+  values = [
+    "${file("${path.module}/templates/prometheus_values.yaml")}"
+  ]
+
+  depends_on = [
+    kubernetes_namespace.prometheus,
+    local_file.prometheus_values
+  ]
+}
