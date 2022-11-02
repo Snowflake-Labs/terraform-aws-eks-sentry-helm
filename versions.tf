00c5c5ee648d45b7e102bd506c34e2f4bd49a0b8
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-11-01 21:31:50 -0700

    Upgrade sentry provider version.

--- versions.tf
+++ versions.tf
@@ -1,8 +1,10 @@
 terraform {
-  required_version = ">= 1.2.0"
+  required_version = "~> 1.2.0"
 
   required_providers {
     aws        = "~> 4.37.0"
+    local      = "~> 1.4.0"
+    random     = "~> 2.1.0"
     kubernetes = "~> 2.15.0"
   }
 }
