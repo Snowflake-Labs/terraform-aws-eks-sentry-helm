019e0a34cedd1bed0c087defecb72e718dcc5aba
Prasanth Kommini <prasanth.kommini@snowflake.com>
2023-07-17 15:22:51 -0700

    Fix allow from CIDR blocks.

--- security.tf
+++ security.tf
@@ -51,7 +51,6 @@ resource "aws_security_group_rule" "sentry_private_ingress_allow_from_private_su
   description       = "Allow from private subnet CIDRs."
 }
 
-
 locals {
   sentry_private_ingress_sg_ids = length(var.allowed_security_group_ids) == 0 ? [] : [aws_security_group.sentry_private_ingress_sg.0.id]
 }
